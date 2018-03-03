a772f8fd2970fd645dbcb7330502ba0357ad44a4
Le Van Toan <letoan285@gmail.com>
2018-03-03 13:37:06 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Helo {{$n}}
+  
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 // 	$thu
 // 	return view('demo');
 // });
+Route::view('/welcome', 'welcome');
